f968e3d5e49170d1a9e0044998543fcfc243d4d4
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-09-26 13:07:25 +0200

    updating index.html file

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,8 +6,8 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
  const appRoutes: Routes = [
-  { path: 'login',  component:  LoginComponent },
-  { path: 'register', component: RegisterComponent },  
+  { path: '',  component:  LoginComponent },
+  { path: 'register', component: RegisterComponent }, 
   { path: '**', component: NotFoundComponent }
  ];
 @NgModule({
