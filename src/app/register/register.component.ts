3d92e19aa02fd3ecf36ee6107294aac6a3e0c762
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-09-26 15:17:06 +0200

    updating register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { FormsService } from '../shared/forms.service';
 
 @Component({
@@ -8,11 +9,22 @@ import { FormsService } from '../shared/forms.service';
 })
 export class RegisterComponent implements OnInit {
   public paises: any;
-  constructor(private _formsService: FormsService) { }
+  public registrationForm: FormGroup;
+  constructor(private _formsService: FormsService, private _fb: FormBuilder) { }
   ngOnInit() {
+    this.createForm();
     this.getCountries();
   } 
-  getCountries( ){ 
+  public createForm( ){
+   this.registrationForm =  this._fb.group({
+            username: ['',Validators.required],
+            email:['',Validators.required],
+            country:['', Validators.required],
+            confirmEmail:['', Validators.required],
+            password: ['', Validators.required]
+    });
+  }
+  private getCountries(){ 
     this._formsService.getCountries()
         .subscribe(paises => this.paises = paises);
   } 
