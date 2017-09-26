a4201907d27b352e73100a10d1bb01b223be335b
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-09-26 03:59:51 +0200

    Creating service that provides the full list of countries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './notfound.component';
 
-import  { RoutesModule } from './routes';
+import { RoutesModule } from './routes';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -17,6 +18,7 @@ import { RegisterComponent } from './register/register.component';
 ],
   imports: [
     BrowserModule, 
+    HttpModule,
     RoutesModule
   ],
   providers: [],
