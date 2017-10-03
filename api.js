a8bd7065dd20b5cf1bf7175a6f973da8bf7dbbae
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-10-03 13:27:35 +0200

    adding server interaction

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const ObjectID = require('mongodb').ObjectID;
 
 // Connect
 const connection = (closure) => {
-    return MongoClient.connect('mongodb://localhost:27017/mean', (err, db) => {
+    return MongoClient.connect('mongodb://localhost:27017/geocity', (err, db) => {
         if (err) return console.log(err);
 
         closure(db);
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const app = express();
 
 // API file for interacting with MongoDB
-const api = require('./server/routes/api');
+const api = require('./routes/api');
 
 // Parsers
 app.use(bodyParser.json());
