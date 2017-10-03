f31cd7ec2f332a57b6b5c14ee174f309d1247b7a
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-10-03 13:48:28 +0200

    deleting extra data

--- client/src/app/shared/authentication.service.ts
+++ client/src/app/shared/authentication.service.ts
@@ -1,14 +1,8 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth , FirebaseAuthStateObservable } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Rx';
-import * as firebase from 'firebase/app';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 @Injectable()
 export class AuthenticationService {
-constructor(private _af: AngularFireAuth) { }
-    public loginUser( email: string, password: string) { 
-        this._af.auth.signInWithEmailAndPassword(email,password);
-    }
-    public logoutUser():any { 
-        this._af.auth.signOut();    
-    }
+constructor() { }
+
 }
