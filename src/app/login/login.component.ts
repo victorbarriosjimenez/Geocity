58da4034b590dbaf969ea95365d66c9f08557b1b
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-10-03 19:28:04 +0200

    launchhing with ng serve

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AlertService } from '../shared/alert.service';
+import { AuthenticationService } from '../shared/authentication.service';
+
+import { Observable } from 'rxjs/Rx'; 
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
+  constructor(private _fb: FormBuilder) { }
+  public createForm( ){
+    this.loginForm =  this._fb.group({
+             email:['',Validators.required],
+             password: ['', Validators.required],
+     });
+   }
+  ngOnInit() {
+    this.createForm();
+  }
+  public login() {  
+    
+  } 
+}
