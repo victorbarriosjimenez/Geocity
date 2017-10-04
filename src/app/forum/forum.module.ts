3ed9f0342b52335838263d53ecee15a03c0bf09d
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-10-04 16:51:01 +0200

    deleted extra line

--- src/app/forum/forum.module.ts
+++ src/app/forum/forum.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ForumComponent } from './forum.component';
-import { ForumRoutesModule } from './forum-routes':;
-
+import { ForumRoutesModule } from './forum-routes';
 @NgModule({
   imports: [
     CommonModule,
