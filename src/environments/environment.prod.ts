0e5e2796f3379243ac003415ac868280f1076243
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-10-17 21:19:33 +0200

    fixing issue with envinronment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+   firebase: {
+    apiKey: "AIzaSyB1aK8uCMZECNN4GulkYsvZDVkSnj4VL9E",
+    authDomain: "geocity-app.firebaseapp.com",
+    databaseURL: "https://geocity-app.firebaseio.com",
+    projectId: "geocity-app",
+    storageBucket: "geocity-app.appspot.com",
+    messagingSenderId: "840348035338"
+  }
 };
