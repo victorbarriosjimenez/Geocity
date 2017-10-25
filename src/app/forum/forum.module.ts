431c953e9ac5d3d062e090c58ddda3050b344e03
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-10-25 19:42:52 +0200

    creating navigation component

--- src/app/forum/forum.module.ts
+++ src/app/forum/forum.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ForumComponent } from './forum.component';
 import { ForumRoutesModule } from './forum-routes';
+import { NavigationComponent } from './Users/vicbarriosjimenez/Desktop/game-project/web-app/src/app/user/navigation/navigation.component';
 @NgModule({
   imports: [
     CommonModule,
     ForumRoutesModule
   ],
   declarations: [
-    ForumComponent
-  ]
+    ForumComponent,
+    NavigationComponent
+]
 })
 export class ForumModule { }
