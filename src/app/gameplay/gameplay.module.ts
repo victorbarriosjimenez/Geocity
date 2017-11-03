093b48f4651478f5b75ffaaa522f51b4cdee88d4
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-11-03 12:26:54 +0100

    updating gameplay component

--- src/app/gameplay/gameplay.module.ts
+++ src/app/gameplay/gameplay.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { GameplayComponent } from './gameplay.component';
 
 import { MaterialElementsModule  } from '../shared/material-elements.module';
+import { AgmCoreModule } from '@agm/core';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialElementsModule
+    MaterialElementsModule,
+    AgmCoreModule
   ],
   declarations: [GameplayComponent],
   exports:[
---
+++ src/app/gameplay/gameplay.routes.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GameplayComponent } from './gameplay.component';
+import { AuthGuard } from '../shared/auth.guard'
+
+const gameplayRoutes: Routes = [
+  { path: 'gameplay',  component: GameplayComponent, canActivate: [AuthGuard] ,  data: { animation: 'gameplay' }}
+];
+@NgModule({
+  imports: [
+    RouterModule.forChild(gameplayRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class ForumRoutesModule { }
