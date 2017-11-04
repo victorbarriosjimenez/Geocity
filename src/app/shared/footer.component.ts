bab74e91587ce6a848a66b257d0c99f55b48e988
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-11-04 16:03:59 +0100

    creating new component

---
+++ src/app/shared/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+    selector: 'app-footer',
+    template: `
+        <p>Footer works</p>
+    `,
+    styles: [`
+    
+    `]
+})
+export class FooterComponent {
+    constructor() { }
+}
