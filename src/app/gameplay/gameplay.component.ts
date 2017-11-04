837c5bb594ae27dc179beaa6ea37df09f82bfe59
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-11-04 18:17:38 +0100

    Creating a form to chose the actual continent

--- src/app/gameplay/gameplay.component.ts
+++ src/app/gameplay/gameplay.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+
+export interface Continent { 
+      name:  string;
+      apiEndpoint: string;
+      lat: number;
+      lon: number;
+}
+
 @Component({
   selector: 'app-gameplay',
   templateUrl: './gameplay.component.html',
   styleUrls: ['./gameplay.component.css']
 })
 export class GameplayComponent implements OnInit {
-  public continentes = [
+  public continents = [
     { "name": "America del Norte", "apiEndpoint": "" },
-    { "name": "America del Sur",   },
-    { "name": "Africa" },
-    { "name": "Asia" },
-    { "name": "Europa" },
-    { "name": "Oceanía", }
-  ] 
+    { "name": "America del Sur", "apiEndpoint":""  },
+    { "name": "Africa", "apiEndpoint":""  },
+    { "name": "Asia", "apiEndpoint":""  },
+    { "name": "Europa" ,"apiEndpoint":"" },
+    { "name": "Oceanía","apiEndpoint":""  }
+  ]; 
   public lat: number;
   public lng: number;
   constructor() { }
