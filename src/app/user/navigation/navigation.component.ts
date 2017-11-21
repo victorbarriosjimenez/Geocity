1a941dd84d958cbde6935ed3ee63a12e06fc8b50
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-11-21 19:27:44 +0100

    Updating the navigation component

--- src/app/user/navigation/navigation.component.ts
+++ src/app/user/navigation/navigation.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from './../../shared/authentication.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
-  constructor(private auth: AuthenticationService) { }
+  routes: any =  [
+    { endPoint: 'profile', tooltip: 'Mi perfil' , icon: 'perm_identity' },
+    { endPoint: 'friends', tooltip: 'Amigos' , icon: 'people' },
+    { endPoint: 'community', tooltip: 'Foro' , icon: 'chat' },
+    { endPoint: 'ranking', tooltip: 'Ranking' , icon: 'sort' } 
+  ];
+  constructor(private auth: AuthenticationService, private _router: Router) { }
   ngOnInit() { }
+  redirectsToRouteSelected(routeSelected: string){
+    this._router.navigate([`/${routeSelected}`])
+  }
   public logoutFromGeocity(): void {
     this.auth.logoutfromGeocity();
   } 
