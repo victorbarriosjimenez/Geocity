f8703f9531fb1a9b603e80f3c836589535045e20
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-11-26 15:04:47 +0100

    Updating component

--- src/app/user/ranking/ranking.component.ts
+++ src/app/user/ranking/ranking.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RankingService } from '../../shared/index';
 import { User } from '../../../models/index';
+import { orderBy } from 'lodash';
 @Component({
     selector: 'ranking',
     templateUrl: 'ranking.component.html',
@@ -15,6 +16,11 @@ export class RankingComponent implements OnInit {
      }
     public getWorldRankingPodium(): void  {
         this._rankingService.getListOfAllUsers()
-            .subscribe(users => {  this.rankedUsers = users });
-      }
+            .subscribe(users => {  
+                this.filterUsers(users);
+            });
+    }
+    public filterUsers(users: User[]){
+       this.rankedUsers = orderBy(users,['score'],['desc']);
+    }
 }
