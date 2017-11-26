cf8a6a1be803c9d2667b54a00f9c278a5c03eeb4
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-11-26 17:20:00 +0100

    Fixing problem that prevented compilation

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,9 +1,11 @@
 export class Post { 
+    public $key?: string;
     public userId?: string;
     public body?: string;
     public timestamp?: any;
     public authorProfilePhoto?: string;
     public authorUsername?: string; 
+    public comments?: Comment[];
 }
 export class Comment { 
     public userId?: string;
