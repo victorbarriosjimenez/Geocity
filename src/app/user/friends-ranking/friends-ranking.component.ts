b4520c3150e167c9eb8679301db8d9ff3aa07bdc
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-11-29 18:57:47 +0100

    Creating the multyquery system

--- src/app/user/friends-ranking/friends-ranking.component.ts
+++ src/app/user/friends-ranking/friends-ranking.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormsService, RankingService } from '../../shared/index';
 import { FormGroup, FormBuilder } from '@angular/forms/';
 import { User } from '../../../models/index';
+import  { filter } from 'lodash';
 @Component({
   selector: 'app-friends-ranking',
   templateUrl: './friends-ranking.component.html',
@@ -9,7 +10,7 @@ import { User } from '../../../models/index';
 })
 export class FriendsRankingComponent implements OnInit {
   public countries: any;
-  public users: User[] = [];
+  public rankedUsers: any[] = [];
   public searchFriendForm : FormGroup
   public username = ''
   public country = ''
@@ -24,10 +25,21 @@ export class FriendsRankingComponent implements OnInit {
         .subscribe(countries => this.countries = countries);
   }
   public filterUsers(){
-    let userquery = this.username;
-    this.rankingService.getListOfAllUsers()
-        .subscribe(users =>{ 
-             this.users = users.filter((user: User) => user.username.toLocaleLowerCase().indexOf(userquery) !==  -1) 
-        });
-  }
+    if(this.rankedUsers){
+       this.rankedUsers= [];
+       let userquery = this.username;
+       let countryquery = this.country;
+       this.rankingService.getListOfAllUsers()
+           .subscribe(users => { 
+               if(countryquery){
+                this.rankedUsers = filter(users.filter((user: User) => user.username.toLowerCase().indexOf(userquery) !== -1),{ country : countryquery }); 
+               }
+               else if(!countryquery && userquery){
+                this.rankedUsers =  users.filter((user: User) => user.username.toLowerCase().indexOf(userquery) !== -1);
+               }
+           }); 
+      }
+    }
+  
+
 }
