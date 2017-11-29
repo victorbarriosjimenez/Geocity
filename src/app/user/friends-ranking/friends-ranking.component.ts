06a810ea84081241b1313282cb5256c67bbe9a99
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-11-29 14:35:25 +0100

    Ceating the form group

--- src/app/user/friends-ranking/friends-ranking.component.ts
+++ src/app/user/friends-ranking/friends-ranking.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormsService } from '../../shared/index';
+import { FormGroup } from '@angular/forms/';
 @Component({
   selector: 'app-friends-ranking',
   templateUrl: './friends-ranking.component.html',
   styleUrls: ['./friends-ranking.component.css']
 })
 export class FriendsRankingComponent implements OnInit {
-
-  constructor() { }
-
+  public countries: any;
+  public searchFriendForm : FormGroup
+  constructor(private formsService: FormsService) { }
   ngOnInit() {
+    this.getListOfCountries();
   }
-
+  public getListOfCountries(){
+    this.formsService.getCountries()
+        .subscribe(countries => this.countries = countries);
+  }
+  public createForm( ): void {
+    this.searchFriendForm =  this._fb.group({
+             usernameFormControl:[''],
+             countryFormControl:[''],
+     });
+   }
 }
