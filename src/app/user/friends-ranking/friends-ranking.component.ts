4ab2997871e84d05047de1ca59a2d9e977f2b6a7
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-11-29 19:58:38 +0100

    Refactoring functions

--- src/app/user/friends-ranking/friends-ranking.component.ts
+++ src/app/user/friends-ranking/friends-ranking.component.ts
@@ -11,35 +11,46 @@ import  { filter } from 'lodash';
 export class FriendsRankingComponent implements OnInit {
   public countries: any;
   public rankedUsers: any[] = [];
-  public searchFriendForm : FormGroup
-  public username = ''
-  public country = ''
-  constructor(private formsService: FormsService,
-              private rankingService: RankingService,
-              private _fb: FormBuilder) { }
+  public username: string = '';
+  public country: string = '';
   ngOnInit() {
     this.getListOfCountries();
   }
+  constructor(private formsService: FormsService,
+              private rankingService: RankingService,
+              private _fb: FormBuilder) { }
   public getListOfCountries(){
     this.formsService.getCountries()
         .subscribe(countries => this.countries = countries);
   }
-  public filterUsers(){
+  public filterByTextControl(): void{
+    let userquery = this.username;
+    let countryquery = this.country;
+    this.rankedUsers = userquery === '' ? [] : this.rankedUsers;
+      if(this.rankedUsers) {
+        this.rankedUsers= [];
+        this.rankingService.getListOfAllUsers()
+            .subscribe(users => { 
+                if(countryquery){
+                    this.rankedUsers = filter(users.filter((user: User) => user.username.toLowerCase().indexOf(userquery) !== -1),{ country : countryquery }); 
+                }
+                else if(!countryquery && userquery){
+                    this.rankedUsers =  users.filter((user: User) => user.username.toLowerCase().indexOf(userquery) !== -1);
+                }
+        }); 
+      }
+  }
+  public filterByCountryControl(): void {
+    let userquery = this.username;
+    let countryquery = this.country;
     if(this.rankedUsers){
        this.rankedUsers= [];
-       let userquery = this.username;
-       let countryquery = this.country;
-       this.rankingService.getListOfAllUsers()
-           .subscribe(users => { 
-               if(countryquery){
-                this.rankedUsers = filter(users.filter((user: User) => user.username.toLowerCase().indexOf(userquery) !== -1),{ country : countryquery }); 
-               }
-               else if(!countryquery && userquery){
-                this.rankedUsers =  users.filter((user: User) => user.username.toLowerCase().indexOf(userquery) !== -1);
-               }
-           }); 
-      }
+       if(userquery){
+        this.rankingService.getListOfAllUsers()
+        .subscribe(users => { 
+             this.rankedUsers = filter(users.filter((user: User) => user.username.toLowerCase().indexOf(userquery) !== -1),{ country : countryquery }); 
+        });
+       }
     }
-  
-
+  }
 }
