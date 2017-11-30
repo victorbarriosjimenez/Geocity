aa139966ca39ec92c613182f67362fea82e06f25
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-11-30 01:23:40 +0100

    Using bingding to switch button icon

--- src/app/shared/follow-button/follow-button.component.ts
+++ src/app/shared/follow-button/follow-button.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from '../index';
 
 @Component({
   selector: 'app-follow-button',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./follow-button.component.css']
 })
 export class FollowButtonComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  @Input('userId') userId: string;
+  @Input('currentUserId') currentUserId: string;
+  public isFollowing: boolean; 
+  public icon: string = 'person_add' 
+  constructor(private _userService: UserService) { }
+  ngOnInit() { }
+  toggleFollow(key: string) {
+    const userId = key;
+    const currentUserId = this.currentUserId;
+    if (this.isFollowing) this._userService.unfollow(currentUserId, userId)
+    else this._userService.follow(currentUserId, userId)
+  }   
 }
--- src/app/user/ranking/ranking.component.ts
+++ src/app/user/ranking/ranking.component.ts
@@ -8,10 +8,6 @@ import { orderBy } from 'lodash';
     styleUrls: ['ranking.component.css']
 })
 export class RankingComponent implements OnInit {
-    followerCount: number;
-    isFollowing: boolean;
-    followers;
-    following;  
     public rankedUsers: User[];
     public term: string = '';
     constructor(private _rankingService: RankingService,
@@ -28,10 +24,4 @@ export class RankingComponent implements OnInit {
     public filterUsers(users: User[]){
        this.rankedUsers = orderBy(users,['score'],['desc']);
     }
-    toggleFollow(key: string) {
-        const userId = key;
-        const currentUserId = this._userService.currentUserId;
-        if (this.isFollowing) this._userService.unfollow(currentUserId, userId)
-        else this._userService.follow(currentUserId, userId)
-    }   
 }
