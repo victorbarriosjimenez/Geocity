2c3d109d3785b11f576731ca8611e84c895ce1ea
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-12-08 21:42:35 +0100

    Fixing issue that prevented compilation

--- src/app/user/friends-list/friends-list.component.ts
+++ src/app/user/friends-list/friends-list.component.ts
@@ -8,56 +8,30 @@ import  { filter, keys, size } from 'lodash';
   styleUrls: ['./friends-list.component.css']
 })
 export class FriendsListComponent implements OnInit {
-  public countries: any;
-  public rankedUsers: any[] = [];
   public username: string = '';
   public country: string = '';
   public friends: User[] = [];
   public followerCount: number;
   ngOnInit() {
     this.getInitialListOfFriends();
-    this.getListOfCountries();
   }
-  constructor(private formsService: FormsService,
+  constructor(
               private rankingService: RankingService,
               private _userService:  UserService) { }
-  public getListOfCountries(){
-    this.formsService.getCountries()
-        .subscribe(countries => this.countries = countries);
-  }
-  public filterByTextControl(): void {
-    let userquery = this.username;
-    let countryquery = this.country;
-    this.rankedUsers = userquery === '' ? [] : this.rankedUsers;
-      if(this.rankedUsers) {
-        this.rankedUsers= [];
-        this.rankingService.getListOfAllUsers()
-            .subscribe(users => { 
-                if(countryquery){
-                    this.rankedUsers = filter(users.filter((user: User) => user.username.toLowerCase().indexOf(userquery) !== -1),{ country : countryquery }); 
-                }
-                else if(!countryquery && userquery){
-                    this.rankedUsers =  users.filter((user: User) => user.username.toLowerCase().indexOf(userquery) !== -1);
-                }
-        }); 
-    }
-  }
-  public filterByCountryControl(): void {
-    let userquery = this.username;
-    let countryquery = this.country;
-    if(this.rankedUsers){
-       this.rankedUsers= [];
-       if(userquery){
-        this.rankingService.getListOfAllUsers()
-        .subscribe(users => { 
-             this.rankedUsers = filter(users.filter((user: User) => user.username.toLowerCase().indexOf(userquery) !== -1),{ country : countryquery }); 
-        });
-       }
-    }
-  }
   public getInitialListOfFriends(): void {
-    this.rankingService.getListOfAllUsers()
-        .subscribe(users => this.rankedUsers = users);
+    this._userService.getFollowingList(this._userService.currentUserId)
+    .subscribe(followers => {
+                this.setFriendKey(followers);
+                this.followerCount = this.countFollowers(followers);
+              });
+  }
+  public setFriendKey(followers): void {
+    keys(followers).map(key => this._userService.getFriendData(key)
+                   .subscribe(friend => this.friends.push(friend.payload.val()), 
+                             (err) => console.log(err),
+                             () => { 
+                              this.friends = this.friends.slice(0,13);
+                            }));
   }
   private countFollowers(followers) {
     if (followers.$value===null) return 0
--- src/app/user/user-search/user-search.component.ts
+++ src/app/user/user-search/user-search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../../models/index';
+import { FormsService } from '../../shared/index';
 
 @Component({
   selector: 'app-user-search',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-search.component.css']
 })
 export class UserSearchComponent implements OnInit {
-
-  constructor() { }
+  public countries: any;
+  public rankedUsers : User[] = [];
+  constructor(private formsService: FormsService) { }
 
   ngOnInit() {
   }
-
+  public getListOfCountries(){
+    this.formsService.getCountries()
+        .subscribe(countries => this.countries = countries);
+  }
+  public filterByTextControl(): void {
+    let userquery = this.username;
+    let countryquery = this.country;
+    this.friends = userquery === '' ? [] : this.rankedUsers;
+      if(this.rankedUsers) {
+        this.rankedUsers= [];
+        this.rankingService.getListOfAllUsers()
+            .subscribe(users => { 
+                if(countryquery){
+                    this.rankedUsers = filter(users.filter((user: User) => user.username.toLowerCase().indexOf(userquery) !== -1),{ country : countryquery }); 
+                }
+                else if(!countryquery && userquery){
+                    this.rankedUsers =  users.filter((user: User) => user.username.toLowerCase().indexOf(userquery) !== -1);
+                }
+        }); 
+    }
+  }
+  public filterByCountryControl(): void {
+    let userquery = this.username;
+    let countryquery = this.country;
+    if(this.rankedUsers){
+       this.rankedUsers= [];
+       if(userquery){
+        this.rankingService.getListOfAllUsers()
+        .subscribe(users => { 
+             this.rankedUsers = filter(users.filter((user: User) => user.username.toLowerCase().indexOf(userquery) !== -1),{ country : countryquery }); 
+        });
+       }
+    }
+  }
 }
