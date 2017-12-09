dd73968f7d395fdab3341c2e2c9adfda9ee56f88
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-12-09 23:16:50 +0100

    Creatoing  theinteraction with notifications

--- src/app/user/notifications-center/notifications-center.component.ts
+++ src/app/user/notifications-center/notifications-center.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../shared/index';
+import { OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-notifications-center',
   templateUrl: './notifications-center.component.html',
   styleUrls: ['./notifications-center.component.css']
 })
-export class NotificationsCenterComponent implements OnInit {
-  public messages: any;
+export class NotificationsCenterComponent implements OnInit, OnChanges {
+  public notifications: any;
   constructor(private _userService: UserService) { }
   ngOnInit() {
       this.getListOfNotifications();
   }
+  ngOnChanges(){
+    this.getListOfNotifications();    
+  }
   public getListOfNotifications( ){
     this._userService.getUserNotifications(this._userService.currentUserId)
-        .subscribe(messages => this.messages = messages);
+        .subscribe(notifications => this.notifications = notifications);
   }
-
+  public deleteNotification(notificationKey: string) {
+    this._userService.deleteNotification(notificationKey).then(() => this.getListOfNotifications());
+  }
 }
