f2d7ce1452797004c1622774b542c836bce417ed
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-12-13 20:46:51 +0100

    Adding the first test

--- src/app/shared/footer/footer.component.spec.ts
+++ src/app/shared/footer/footer.component.spec.ts
@@ -6,8 +6,22 @@ import { DebugElement } from '@angular/core';
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
-  
-  
+  describe('FooterComponent', () => {
+      let component: FooterComponent;
+      let fixture: ComponentFixture<FooterComponent>;
+      beforeEach(async(() => {
+        TestBed.configureTestingModule({
+          declarations: [ FooterComponent ]
+        })
+        .compileComponents();
+      }));
+      beforeEach(() => {
+        fixture = TestBed.createComponent(FooterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+      });
+      it('should create', () => {
+        expect(component).toBeTruthy();
+      });
+    }); 
 });
