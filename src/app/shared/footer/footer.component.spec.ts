c292828f0183f89198b42525b89ffebb1ec737c7
victorbarriosjimenez <victorhugo.barrios.jimenez@alumnos.upm.es>
2017-12-16 20:14:17 +0100

    Updating the footer component

--- src/app/shared/footer/footer.component.spec.ts
+++ src/app/shared/footer/footer.component.spec.ts
@@ -2,26 +2,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { UserService } from '../index';
+describe('ForumComponent', () => {
 
-import { FooterComponent } from './footer.component';
-
-describe('FooterComponent', () => {
-  describe('FooterComponent', () => {
-      let component: FooterComponent;
-      let fixture: ComponentFixture<FooterComponent>;
-      beforeEach(async(() => {
-        TestBed.configureTestingModule({
-          declarations: [ FooterComponent ]
-        })
-        .compileComponents();
-      }));
-      beforeEach(() => {
-        fixture = TestBed.createComponent(FooterComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-      });
-      it('should create', () => {
-        expect(component).toBeTruthy();
-      });
-    }); 
-});
+}); 
--- src/app/user/user-search/user-search.component.spec.ts
+++
@@ -1,9 +0,0 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
-import { UserSearchComponent } from './user-search.component';
-
-describe('UserSearchComponent', () => {
-  
-});
